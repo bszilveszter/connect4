faf969bbc31c9c323584023cf779169ac562ccf5
Szilveszter Barna <barnaszilveszter@hotmail.com>
2024-10-17 19:28:22 +0200

    not final yet

---
+++ src/main/java/org/bsz/connect4/Main.java
@@ -0,0 +1,13 @@
+package org.bsz.connect4;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize players: human and computer
+        Player human = new Player("Human", 'Y', true);
+        Player computer = new Player("Computer", 'R', false);
+
+        // Create and start the game
+        Game game = new Game(human, computer);
+        game.start();
+    }
+}
